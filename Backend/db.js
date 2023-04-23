9233eeea3235a4086d11374b7707aec06fa11644
karandeep-3154 <96348619+karandeep-3154@users.noreply.github.com>
2023-04-23 10:39:45 +0530

    Update db.js

--- Backend/db.js
+++ Backend/db.js
@@ -1,14 +1,12 @@
 const mongoose = require("mongoose")
-const mongoURI = "mongodb+srv://ksingh3154:polpol@098@cluster0.zvgdzep.mongodb.net/?retryWrites=true&w=majority";
+const url = "mongodb+srv://ksingh3154:polpol@098@cluster0.zvgdzep.mongodb.net/?retryWrites=true&w=majority";
 
 
 const connectToMongo = ()=>{
-    mongoose.connect(mongoURI,{serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }}
-    )
+  mongoose.set("strictQuery", false);  //for removing deprecation warning
+    mongoose.connect(url , 
+        { useNewUrlParser: true, useUnifiedTopology: true },
+        ()=>{  console.log(`Connected with database Successfuly`); })
 }
 
 module.exports = connectToMongo;
