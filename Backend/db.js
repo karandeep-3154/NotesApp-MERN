cd03855e6e19fafb722d521cf55f1346f7e1e9ca
karandeep-3154 <96348619+karandeep-3154@users.noreply.github.com>
2023-04-23 10:31:28 +0530

    Update db.js

--- Backend/db.js
+++ Backend/db.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose")
-const mongoURI = "mongodb+srv://ksingh3154:polpol@098@cluster0.zvgdzep.mongodb.net/notesapp";
+const mongoURI = "mongodb+srv://ksingh3154:polpol@098@cluster0.zvgdzep.mongodb.net/?retryWrites=true&w=majority";
+
 
 const connectToMongo = ()=>{
     mongoose.connect(mongoURI,
