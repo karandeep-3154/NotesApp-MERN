1c87bc70f020b5c78afa51ccff6f94148b81401b
karandeep-3154 <96348619+karandeep-3154@users.noreply.github.com>
2023-04-23 10:16:11 +0530

    Update NoteState.js

--- Frontend/src/Context/Notes/NoteState.js
+++ Frontend/src/Context/Notes/NoteState.js
@@ -2,7 +2,7 @@ import NoteContext from "./Notecontext";
 import { useState } from "react";
 
 const NoteState = (props) => {
-  const host = "http://localhost:5000"
+  const host = "https://notesapp-mern.onrender.com"
   const notesInitial = []
   const [notes, setNotes] = useState(notesInitial)
 
